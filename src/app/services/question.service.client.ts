185059cbbc934a9ce43e43771f6e69c5638c17ab
Gaurav Kohli <gauravkohli174@gmail.com>
2020-04-22 14:33:57 -0400

    added question service client

---
+++ src/app/services/question.service.client.ts
@@ -0,0 +1,9 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+
+export class QuestionServiceClient {
+  findQuestionsForQuiz = (qid) =>
+    fetch(`https://cs5610-gk-a8-server.herokuapp.com/api/quizzes/${qid}/questions`)
+      .then(response => response.json())
+}
